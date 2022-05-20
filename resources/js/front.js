bb5e3368db1504c24912de18de7126fa20a91f9c
edodima13 <edodima13@libero.it>
2022-05-20 18:20:57 +0200

    installazione e configurazione

--- resources/js/front.js
+++ resources/js/front.js
@@ -14,7 +14,10 @@
  window.Vue = require('vue');
 
  import App from './views/App.vue'
- 
+ import VueRouter from 'vue-router'
+ import Vue from 'vue';
+
+Vue.use(VueRouter)
 
 const app = new Vue({
     el: '#root',
